9a4b2974d4566d3eedb11de86dad57fafcbedb33
Amman Zaman <amman.z.zaman@gmail.com>
2021-01-10 02:21:11 -0800

    added fullscreen behaviour

--- lib/src/screens/quran_pager.dart
+++ lib/src/screens/quran_pager.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fullscreen/fullscreen.dart';
 
 class QuranPager extends StatefulWidget {
   final int initialPage;
@@ -9,27 +10,48 @@ class QuranPager extends StatefulWidget {
 
 class QuranPagerState extends State<QuranPager> {
   PageController pageController;
+  FullScreen fullscreen;
 
   initState() {
     super.initState();
     pageController = PageController(initialPage: widget.initialPage);
+    fullscreen = new FullScreen();
+    fullscreen.enterFullScreen(FullScreenMode.EMERSIVE);
   }
 
   Widget build(context) {
-    return PageView.builder(
-      controller: pageController,
-      reverse: true,
-      itemCount: 604,
-      itemBuilder: (context, index) => Container(
-        alignment: Alignment.center,
-        color: Colors.white,
-        child: Image(
-          image: AssetImage(
-              'lib/res/ClassicMadani15/classicmadani15_pg_${index + 1}.png'),
-          fit: BoxFit.contain,
-          filterQuality: FilterQuality.high,
+    return WillPopScope(
+      child: GestureDetector(
+        onTap: () {
+          fullscreen.isFullScreen.then((isFullScreen) {
+            isFullScreen
+                ? fullscreen.exitFullScreen()
+                : fullscreen.enterFullScreen(FullScreenMode.EMERSIVE);
+          });
+        },
+        child: PageView.builder(
+          controller: pageController,
+          reverse: true,
+          itemCount: 604,
+          itemBuilder: (context, index) => Container(
+            alignment: Alignment.center,
+            color: Colors.white,
+            child: Image(
+              image: AssetImage(
+                  'lib/res/ClassicMadani15/classicmadani15_pg_${index + 1}.png'),
+              fit: BoxFit.contain,
+              filterQuality: FilterQuality.high,
+            ),
+          ),
         ),
       ),
+      onWillPop: () async {
+        bool isFullScreen = await fullscreen.isFullScreen;
+        if (isFullScreen) {
+          await fullscreen.exitFullScreen();
+        }
+        return true;
+      },
     );
   }
 }
